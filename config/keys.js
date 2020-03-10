4df207cabf112f0e17b1a7ddef9cb66debdfd2fc
John Paul Perez <jp11perez@gmail.com>
2020-03-10 16:31:26 +0800

    edited

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    google:{
+        clientID: '323340565803-915nme061lhb8mvho8uklbl3ttfsr840.apps.googleusercontent.com',
+        clientSecret: 'pdcQPL86Zc4xU1QODQIRN01S'
+    }
+}
